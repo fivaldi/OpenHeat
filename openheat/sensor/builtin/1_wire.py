7ed3c0b0fdc38e8b6ab3ba14237c7b14f0f44ca0
Filip Valder <filip.valder@gmail.com>
2021-01-08 09:24:43 +0100

    WiP 1_wire sensor

--- openheat/sensor/builtin/1_wire.py
+++ openheat/sensor/builtin/1_wire.py
@@ -1,4 +1,8 @@
+import sched
 import time
+from datetime import datetime, timedelta
+
+from w1thermsensor import W1ThermSensor
 
 from openheat.logger import log
 
@@ -7,11 +11,20 @@ class OneWire:
     def __init__(self, config, openheat_data):
         self.config = config
         self.openheat_data = openheat_data
+        self.scheduler = sched.scheduler(time.time, time.sleep)
+        self.sensor = W1ThermSensor()
+
+    def _store_temperature_to_data(self):
+        self.openheat_data['sensors'][self.config['name']] = (datetime.now(),
+                                                              self.sensor.get_temperature())
 
     def start(self):
+        self.scheduler.enter(0, 1, self._store_temperature_to_data)
+        self.scheduler.run(blocking=True)
         while True:
-            log.info(self.config)
-            time.sleep(600)
+            self.scheduler.enter(timedelta(self.config['interval']).total_seconds(), 1,
+                                 self._store_temperature_to_data)
+            log.info(self.openheat_data)
 
 
 _sensor_class = OneWire
