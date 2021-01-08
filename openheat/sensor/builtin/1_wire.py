556c14dd89508c08e927d9ab73088fce664aff93
Filip Valder <filip.valder@gmail.com>
2021-01-08 21:33:27 +0100

    Fix OneWire configuration and scheduler

--- openheat/sensor/builtin/1_wire.py
+++ openheat/sensor/builtin/1_wire.py
@@ -11,12 +11,10 @@ from openheat.utils import config_str_to_timedelta
 class OneWire:
     def __init__(self, config, openheat_data):
         self.config = config
+        self.config['interval'] = config_str_to_timedelta(config['interval'])
         self.openheat_data = openheat_data
         self.scheduler = sched.scheduler(time.time, time.sleep)
         self.sensor = W1ThermSensor()
-
-        for sensor in self.config:
-            sensor['interval'] = config_str_to_timedelta(sensor['interval'])
 
     def _store_temperature_to_data(self):
         self.openheat_data['sensors'][self.config['name']] = (datetime.now(),
@@ -24,12 +22,11 @@ class OneWire:
 
     def start(self):
         self.scheduler.enter(0, 1, self._store_temperature_to_data)
-        self.scheduler.run(blocking=True)
         while True:
-            self.scheduler.enter(timedelta(self.config['interval']).total_seconds(), 1,
-                                 self._store_temperature_to_data)
-            log.info(self.config)
+            self.scheduler.run(blocking=True)
             log.info(self.openheat_data)
+            self.scheduler.enter(self.config['interval'].total_seconds(), 1,
+                                 self._store_temperature_to_data)
 
 
 _sensor_class = OneWire
