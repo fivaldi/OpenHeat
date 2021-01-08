35db96817eddf62eeef73e8b02f3f78f769baa04
Filip Valder <filip.valder@gmail.com>
2021-01-08 09:46:15 +0100

    WiP 1_wire sensor continued...

--- openheat/utils.py
+++ openheat/utils.py
@@ -1,2 +1,16 @@
+from datetime import timedelta
+
+from openheat.exceptions import ConfigError
+
+
 def clamp(n, minn, maxn):
     return minn if n < minn else maxn if n > maxn else n
+
+
+def config_str_to_timedelta(config_str):
+    try:
+        kwarg_name, kwarg_value = config_str.split('=')
+        kwarg_value = float(kwarg_value)
+        return timedelta(**{kwarg_name: kwarg_value})
+    except Exception:
+        raise ConfigError(f"Error while converting configuration value {config_str} to timedelta.")
