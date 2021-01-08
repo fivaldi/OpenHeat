e3d295e0b140829e762cd26bfb9c42788c558062
Filip Valder <filip.valder@gmail.com>
2021-01-08 22:32:42 +0100

    Subclass sensors from GenericSensor

--- openheat/sensor/builtin/1_wire.py
+++ openheat/sensor/builtin/1_wire.py
@@ -1,32 +1,18 @@
-import sched
-import time
 from datetime import datetime
 
 from w1thermsensor import W1ThermSensor
 
-from openheat.logger import log
-from openheat.utils import config_str_to_timedelta
+from openheat.sensor import GenericSensor
 
 
-class OneWire:
+class OneWire(GenericSensor):
     def __init__(self, config, openheat_data):
-        self.config = config
-        self.config['interval'] = config_str_to_timedelta(config['interval'])
-        self.openheat_data = openheat_data
-        self.scheduler = sched.scheduler(time.time, time.sleep)
+        super().__init__(config, openheat_data)
         self.sensor = W1ThermSensor()
 
     def _store_temperature_to_data(self):
         self.openheat_data['sensors'][self.config['name']] = (datetime.now(),
                                                               self.sensor.get_temperature())
-
-    def start(self):
-        self.scheduler.enter(0, 1, self._store_temperature_to_data)
-        while True:
-            self.scheduler.run(blocking=True)
-            log.info(self.openheat_data)
-            self.scheduler.enter(self.config['interval'].total_seconds(), 1,
-                                 self._store_temperature_to_data)
 
 
 _sensor_class = OneWire
